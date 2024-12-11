4d97bff30efc492cd8d3c273d3243b372f2b52c0
Vivek Kulkarni <148371774+vivek6593@users.noreply.github.com>
2024-12-11 22:56:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/login', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
